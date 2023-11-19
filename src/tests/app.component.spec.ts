ce2fa34ba2a53d5a2de18c6c4f02be03593f4a4d
Orgiel CE <orgielce@gmail.com>
2023-11-19 14:02:45 -0500

    tests(tests): add tests for app, layout and header components

---
+++ src/tests/app.component.spec.ts
@@ -0,0 +1,51 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from '../app/app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
+import {HeaderComponent} from "../app/components";
+
+
+describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule,
+      BrowserModule,
+      // BrowserAnimationsModule,
+      HttpClientModule,
+      HeaderComponent
+    ],
+    declarations: [AppComponent]
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+  });
+
+  test('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  test(`should have as title 'Challenge products by Orgiel'`, () => {
+    expect(component.title).toEqual('Challenge products by Orgiel');
+  });
+
+  // test(`should found ngx-spinner'`, () => {
+  //   const ngxSpinner = compiled.querySelector('ngx-spinner');
+  //   expect( ngxSpinner ).toBeTruthy();
+  // });
+
+  test('should match with snapshot', () => {
+    fixture.detectChanges();
+    expect(compiled).toMatchSnapshot();
+  });
+});
