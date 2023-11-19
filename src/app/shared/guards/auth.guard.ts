05875439b1bef4125afb94eef611043b55dde865
Orgiel CE <orgielce@gmail.com>
2023-11-19 16:28:51 -0500

    feat(app/shared/guards): add auth guard

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import {AuthService} from "../services";
+import {GlobalState} from "../store";
+import {Store} from "@ngrx/store";
+import {CurrentUser} from "../models";
+import {ROUTES_PATH} from "../routes";
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+
+  private access_token!: CurrentUser['access_token'];
+
+  constructor(private router: Router,
+              private authService: AuthService,
+              private store: Store<GlobalState>,) {
+    this.store.select((store) => store.authentication.currentUser)
+      .subscribe(st => {
+        try {
+          if (st && st.access_token) {
+            this.access_token = st?.access_token;
+          }
+        } catch (e) {
+          throw e;
+        }
+      });
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
+    if (!this.access_token) {
+      this.router.navigate(["/" + ROUTES_PATH.Login]).then();
+      return false;
+    }
+    return true;
+
+  }
+}
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -4,3 +4,4 @@ export * from './models';
 export * from './routes';
 export * from './services';
 export * from './store';
+export * from './guards';
