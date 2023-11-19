ecd419d602a4ee6e1d175b2e6f03aea133e49f90
Orgiel CE <orgielce@gmail.com>
2023-11-19 15:20:14 -0500

    test(tests/components/header): update test

--- src/tests/components/header.component.spec.ts
+++ src/tests/components/header.component.spec.ts
@@ -2,19 +2,35 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {HeaderComponent} from "../../app/components";
 
 describe('HeaderComponent', () => {
+
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let compiled: HTMLElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeaderComponent]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('should have dom elements', () => {
+    fixture.detectChanges();
+    // console.log(compiled.innerHTML);
+    const headerContainer = compiled.querySelector('.header-container');
+    const logo = compiled.querySelector('img');
+
+    expect( headerContainer ).toBeTruthy();
+    expect( logo ).toBeTruthy();
   });
 });
