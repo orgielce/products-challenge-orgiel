cb2fdbd8d964d4f35b2a72ea715901a23d1ecb18
Orgiel CE <orgielce@gmail.com>
2023-11-20 10:09:30 -0500

    config(global): configure primeng

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ROUTES_PATH} from "../../shared";
+import {AuthService, CurrentUser, ROUTES_PATH} from "../../shared";
 import {RouterLink} from "@angular/router";
 
 @Component({
@@ -12,4 +12,11 @@ import {RouterLink} from "@angular/router";
 })
 export class HeaderComponent {
   customRoutes = ROUTES_PATH;
+  user!: CurrentUser;
+
+  constructor(private authService: AuthService) {
+    this.user = authService.currentUserValue;
+  }
+
+  isLogged = (): boolean => this.user.access_token.length > 0
 }
