a1a53d9283644f7103e7d721f305d2b886efa700
Orgiel CE <orgielce@gmail.com>
2023-11-21 16:10:26 -0500

    fix(app/componets/table): update products table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {NgIf} from "@angular/common";
+import {NgClass, NgFor, NgIf} from "@angular/common";
 
 import {TableModule} from "primeng/table";
 import {TooltipModule} from "primeng/tooltip";
@@ -9,12 +9,16 @@ import {Product} from "../../shared";
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [NgIf, TableModule, TooltipModule],
+  imports: [NgIf, NgFor, TableModule, TooltipModule, NgClass],
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent {
 
   @Input() products!: Product[];
+
+  trackByIndex = (index: number): number => {
+    return index;
+  };
 
 }
